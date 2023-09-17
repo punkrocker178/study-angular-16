c37c1cf0419b1303668bebe1d6c16831e9a2d00f
punkrocker178 <ngochieu.le1708@gmail.com>
2023-09-17 10:48:57 +0700

    Test signal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Injector, WritableSignal, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  public count: WritableSignal<number> = signal(0);
+  public currentCount = effect(() => {
+    console.log(`current: ${this.count()}`);
+  });
+
   constructor(
-    private readonly _httpClient: HttpClient
-  ) {
+    private readonly _httpClient: HttpClient,
+    private readonly _injector: Injector
+  ) {}
 
+  ngOnInit(): void {}
 
+  increment(): void {
+    this.count.update(count => count + 1);
   }
 }
